900dab953c1cc106c63bb7702251feae46b89718
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    sm8350-common: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- common.mk
+++ common.mk
@@ -260,21 +260,17 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
-    libqdMetaData \
-    libtinyxml \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     init.qti.display_boot.sh \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
+    libqdMetaData \
     libqdMetaData.system \
     libsdmcore \
     libsdmutils \
-    memtrack.default \
     libmemutils \
+    libtinyxml \
     vendor.display.config@1.5 \
     vendor.display.config@1.15.vendor \
     vendor.display.config@2.0 \
@@ -286,7 +282,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 # DRM
 PRODUCT_PACKAGES += \
