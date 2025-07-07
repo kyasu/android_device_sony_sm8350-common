LineageOS/android_device_oneplus_sm8350-common

sm8350-common: Switch to Touch AIDL
Change-Id: I92a19fedb9ea482616c2e3d550e30b69d2a3a5c7

common.mk
@@ -315,7 +315,7 @@ PRODUCT_PACKAGES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.oplus
+    vendor.lineage.touch-service.oplus
 
 # Update engine
 PRODUCT_PACKAGES += \

kyasu/android_device_sony_sm8350-common

common.mk
@@ -369,7 +369,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.sony
+    vendor.lineage.touch-service.sony
 
 # Update engine
 PRODUCT_PACKAGES += \