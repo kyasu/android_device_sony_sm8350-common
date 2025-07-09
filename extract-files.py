48fd19e679b13d168095834375f962ae5e723f00
basamaryan <basam.aryan@gmail.com>
2025-07-09 08:52:21 -0400

    sm8350-common: Update WFD system blobs from dada OS2.0.217.0.WOCMIXM
    Change-Id: I95dfb07cae97f3c8716a7cb6d87a7081de986748

--- extract-files.py
+++ extract-files.py
@@ -65,24 +65,6 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     (
-        'system_ext/bin/wfdservice',
-    ): blob_fixup()
-        .add_needed('libwfdservice_shim.so'),
-    (
-        'system_ext/lib/libwfdmmsrc_system.so',
-    ): blob_fixup()
-        .add_needed('libgui_shim.so'),
-    (
-        'system_ext/lib/libwfdservice.so',
-    ): blob_fixup()
-        .replace_needed('android.media.audio.common.types-V3-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
-    (
-        'system_ext/lib64/libwfdnative.so',
-    ): blob_fixup()
-        .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
-        .add_needed('libbinder_shim.so')
-        .add_needed('libinput_shim.so'),
-    (
         'vendor/bin/hw/vendor.semc.hardware.extlight-service.somc',
         'vendor/lib64/libcammw.so',
         'vendor/lib64/vendor.semc.hardware.extlight-V1-ndk_platform.so',
