e520a523cf0e1d7e1e14b8b2b260f6f351b6e5b7
kyasu <kyasu.dev@gmail.com>
2026-01-12 13:41:53 +0900

    sm8350-common: init.qcom.msim.sh: Find the exact japanese model
    Set the FeliCa (NFC Type-F) configuration.
    
    Change-Id: Id15c1726faf31087cb32286aecf2511cec98f271

--- rootdir/init.qcom.msim.sh
+++ rootdir/init.qcom.msim.sh
@@ -2,6 +2,10 @@
 
 model=`grep -aim1 'model:' /dev/block/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
+if [ "$model" = "" ]; then
+    model=`grep -aEm1 '(A101SO|A103SO|SOG03|SOG05|SO-51B|SO-53B))&nbsp;' /dev/block/by-name/LTALabel 2> /dev/null | sed -nE 's/.*((A101SO|A103SO|SOG03|SOG05|SO-51B|SO-53B))&nbsp;.*/\1/p'`
+fi
+
 case "$model" in
     "XQ-BC42" | "XQ-BC52" | "XQ-BC62" | "XQ-BC72" | "XQ-BQ42" | "XQ-BQ52" | "XQ-BQ62" | "XQ-BQ72")
         setprop vendor.radio.hardware.sku ds;;
